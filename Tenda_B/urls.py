1bfff95373b42487f22784187c08ac1c8620e842
4Tipy <lchavez21@jaumebalmes.net>
2023-05-04 19:11:26 +0200

    intento de url al index

--- Tenda_B/urls.py
+++ Tenda_B/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('catalog.urls')),
+    path('',  include),
+    path('catalogo/', include('catalog.urls')),
     path('cart/', include('cart.urls')),
     path('admin/', admin.site.urls),
 ]
--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'productos'
 
 urlpatterns = [
-    path('catalogo',views.catalogo, name='catalogo'),
+    path('',views.catalogo, name='catalogo'),
     path('crear/', views.crear_producto, name='crear_producto'),
     path('<int:producto_id>/', views.ver_producto, name='ver_producto'),
     path('<int:producto_id>/eliminar/', views.eliminar_producto, name='eliminar_producto'),
